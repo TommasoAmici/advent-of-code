fb070db45fd226739f817304d25735fd04d9f6e4
Tommaso Amici <me@tommasoamici.com>
2021-12-01 11:45:29 +0100

    feat: day 1 solution

---
+++ day-1/day-1.hs
@@ -0,0 +1,52 @@
+main :: IO ()
+main = do
+  depths <- readDepths
+  -- part 1
+  let increases = depthIncreases depths
+  let totalIncreases = sumBools increases
+  print totalIncreases
+  -- part 2
+  let windows = sumsSlidingWindow depths
+  let windowIncreases = depthIncreases windows
+  let totalWindowIncreases = sumBools windowIncreases
+  print totalWindowIncreases
+
+type Depth = Int
+
+readDepths :: IO [Depth]
+readDepths = do
+  input <- readFile "input.txt"
+  let allLines = lines input
+  let depths = map read allLines :: [Depth]
+  return depths
+
+sumBools :: [Bool] -> Int
+sumBools bools = sum (map (\x -> if x then 1 else 0) bools)
+
+-- part 1, measure increase from previous measure
+depthIncreases :: [Depth] -> [Bool]
+depthIncreases depths = do
+  change <- depthChanges depths
+  let increased = snd change > fst change
+  return increased
+
+depthChanges :: [Depth] -> [(Depth, Depth)]
+depthChanges depths = zip depths depthsOffset
+  where
+    depthsOffset = mconcat [tail depths, [0]]
+
+-- part 2, measure increase from previous sliding window
+sumsSlidingWindow :: [Depth] -> [Depth]
+sumsSlidingWindow depths = map sum windowed
+  where
+    windowed = slidingWindow depths
+
+windowSize :: Int
+windowSize = 3
+
+slidingWindow :: [Depth] -> [[Depth]]
+slidingWindow [] = []
+slidingWindow depths = window : slidingWindow next
+  where
+    window = take windowSize depths
+    next = tail depths
