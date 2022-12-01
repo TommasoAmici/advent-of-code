a97345ddfe4104c5d4803157962f788581ea8f3d
Tommaso Amici <me@tommasoamici.com>
2022-12-01 09:48:47 +0100

    chore: updated template

--- 2022/template.rs
+++ 2022/template.rs
@@ -1,19 +1,24 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
-fn read_file(file_path: &String) -> io::Result<()> {
-    let file = File::open(file_path)?;
-    let reader = BufReader::new(file);
+fn todo(reader: BufReader<File>) {
     for _line in reader.lines() {
         let line = match _line {
             Ok(l) => l,
             Err(error) => panic!("Can't read line: {:?}", error),
         };
-        // XXX
+        println!("{}", line);
     }
+}
+
+fn solve(file_path: &str) -> io::Result<()> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    todo(reader);
     Ok(())
 }
 
 fn main() {
-    read_file(file_path);
+    let file_path = "./data/input.txt";
+    solve(file_path).unwrap();
 }
